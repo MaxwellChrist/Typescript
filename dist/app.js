d5000357bdd5d93b224f509e8c971763b421dc41
MaxwellChrist <ribthulu@gmail.com>
2022-09-12 10:11:06 -0400

    using app.ts to learn about class basics in ts

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,38 @@ const rester = (...nums) => {
 };
 const addRester = rester(5, 10, 15, 20, 25);
 console.log(addRester);
+class Dept {
+    constructor(n) {
+        this.name = n;
+    }
+    describe() {
+        console.log("Department: " + this.name);
+    }
+}
+const security = new Dept("Security");
+security.describe();
+const securityCopy = { name: "copy", describe: security.describe };
+securityCopy.describe();
+class Dept2 {
+    constructor(name, YoE) {
+        this.name = name;
+        this.YoE = YoE;
+        this.employees = [];
+    }
+    describe() {
+        console.log("Department: " + this.name + ". Years of experience: " + this.YoE);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const marketing = new Dept2("Marketing", 1);
+marketing.addEmployee("Max");
+marketing.addEmployee("Sam");
+marketing.describe();
+marketing.printEmployeeInfo();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,39 @@
 const rester = (...nums: number[]) => {
     return nums.reduce((a, b) => a + b, 0)
 }
-
 const addRester = rester(5, 10, 15, 20, 25)
 console.log(addRester)
 
+class Dept {
+    name: string;
+    constructor(n: string) {
+        this.name = n
+    }
+    describe(this: Dept) {
+        console.log("Department: " + this.name)
+    }
+}
+const security = new Dept("Security")
+security.describe()
+const securityCopy = { name: "copy", describe: security.describe }
+securityCopy.describe()
+
+class Dept2 {
+    private employees: string[] = []
+    constructor(private name: string, private readonly YoE: number) {}
+    describe(this: Dept2) {
+        console.log("Department: " + this.name + ". Years of experience: " + this.YoE)
+    }
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+    printEmployeeInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees)
+    }
+}
+const marketing = new Dept2("Marketing", 1);
+marketing.addEmployee("Max");
+marketing.addEmployee("Sam");
+marketing.describe()
+marketing.printEmployeeInfo()
