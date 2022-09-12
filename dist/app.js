9c3f9095aa5b3f154a7331dc048ce167831dac1f
MaxwellChrist <ribthulu@gmail.com>
2022-09-11 20:47:01 -0400

    freshened up on es6 syntax/features used with typescript

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,7 @@
 "use strict";
-const username = "max";
-console.log(username);
+const rester = (...nums) => {
+    return nums.reduce((a, b) => a + b, 0);
+};
+const addRester = rester(5, 10, 15, 20, 25);
+console.log(addRester);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,7 @@
-const username = "max"
-console.log(username)
+const rester = (...nums: number[]) => {
+    return nums.reduce((a, b) => a + b, 0)
+}
+
+const addRester = rester(5, 10, 15, 20, 25)
+console.log(addRester)
+
