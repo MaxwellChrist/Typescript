b6e3b7b87db8e641f7994cb0579a79f01b0e0624
MaxwellChrist <ribthulu@gmail.com>
2022-09-16 13:33:59 -0400

    worked in app.ts learning about generics

--- dist/app.js
+++ dist/app.js
@@ -12,4 +12,51 @@ function mergeObjs(objA, objB) {
     return Object.assign(objA, objB);
 }
 const merged = mergeObjs({ name: "Max" }, { age: 31 });
+function countAndPrint(element) {
+    let description = "Got no value";
+    if (element.length === 1) {
+        description = "Got 1 element";
+    }
+    else if (element.length > 1) {
+        description = "Got " + element.length + " elements";
+    }
+    return [element, description];
+}
+console.log(countAndPrint("Hi there!"));
+console.log(countAndPrint([]));
+console.log(countAndPrint(["Sports", "Cooking"]));
+function extractAndConvert(obj, key) {
+    return "Value: " + obj[key];
+}
+console.log(extractAndConvert({ name: "Melissa" }, "name"));
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem("Max");
+textStorage.addItem("Ali");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+const numberStorate = new DataStorage();
+function createCourseGoal(title, description, date) {
+    let courseGoal = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal;
+}
+const babyNames = ["Leo", "Danny"];
+babyNames.push("Sally");
+babyNames.pop();
 //# sourceMappingURL=app.js.map
