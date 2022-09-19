ee772d5bf32b903c9d34f9e0c7dd633bd8d81d93
MaxwellChrist <ribthulu@gmail.com>
2022-09-19 17:34:35 -0400

    created the necessary methods to submit the form as well as an autobind decorator to not have to bind the this keyword to the class for every method that needs to use it that way

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,50 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// autobind decorator
+function autobind(target, methodName, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
 class ProjectInput {
     constructor() {
         this.template = document.getElementById("project-input");
         this.host = document.getElementById("app");
         const importedNode = document.importNode(this.template.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = "user-input";
+        this.title = this.element.querySelector("#title");
+        this.description = this.element.querySelector("#description");
+        this.people = this.element.querySelector("#people");
+        this.configure();
         this.attach();
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        console.log(this.title.value);
+    }
+    configure() {
+        // the autobind decorator above removes the need for the below code
+        // this.element.addEventListener('submit', this.onSubmit.bind(this))
+        this.element.addEventListener('submit', this.onSubmit);
     }
     attach() {
         this.host.insertAdjacentElement("afterbegin", this.element);
     }
 }
+__decorate([
+    autobind
+], ProjectInput.prototype, "onSubmit", null);
 const projectInput1 = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,24 @@
+// autobind decorator
+function autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        }
+    }
+    return adjDescriptor
+}
+
+
 class ProjectInput {
     template: HTMLTemplateElement
     host: HTMLDivElement
     element: HTMLFormElement
+    title: HTMLInputElement
+    description: HTMLInputElement
+    people: HTMLInputElement
 
     constructor() {
         this.template = document.getElementById("project-input") as HTMLTemplateElement
@@ -9,7 +26,26 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.template.content, true)
         this.element = importedNode.firstElementChild as HTMLFormElement
+        this.element.id = "user-input"
+
+        this.title = this.element.querySelector("#title") as HTMLInputElement
+        this.description = this.element.querySelector("#description") as HTMLInputElement
+        this.people = this.element.querySelector("#people") as HTMLInputElement
+
+        this.configure()
         this.attach()
+    }
+
+    @autobind
+    private onSubmit(e: Event) {
+        e.preventDefault()
+        console.log(this.title.value)
+    }
+
+    private configure() {
+        // the autobind decorator above removes the need for the below code
+        // this.element.addEventListener('submit', this.onSubmit.bind(this))
+        this.element.addEventListener('submit', this.onSubmit)
     }
 
     private attach() {
